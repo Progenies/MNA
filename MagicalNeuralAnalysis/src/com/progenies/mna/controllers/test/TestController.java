d6764f8a2679c067ef0397573fab86f54bad1313
David Luna <ofc587a87@gmail.com>
2012-12-15 20:20:29 +0100

    STARTED - task 41275741: Debe existir una aplicación marco CURRENT

---
+++ MagicalNeuralAnalysis/src/com/progenies/mna/controllers/test/TestController.java
@@ -0,0 +1,17 @@
+package com.progenies.mna.controllers.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("MNA")
+public class TestController
+{
+	@RequestMapping("/hello/{name}")
+    public String hello(@PathVariable String name, Model model) {
+            model.addAttribute("name", name);
+            return "hello/hello";
+    }
+}
